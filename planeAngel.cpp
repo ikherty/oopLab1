a801555024c43e0b5b6b73c0ea64cdc69ae3f17c
Petrenko V. IS-B17 <petrenko@ismag08l.campus.iate.obninsk.ru>
2019-02-28 11:23:18 +0300

    make some changes

--- planeAngel.cpp
+++ planeAngel.cpp
@@ -3,13 +3,58 @@
 
 planeAngel::planeAngel(){}
 
-void planeAngel::Init(int radian, int degree){
+
+void planeAngel::convert(){
+    if(degree!=0||radian!=0){
+        if(degree==0)
+            degree=(int)radian*180/M_PI;
+        if(radian==0)
+            radian=M_PI*degree/180;
+    }
+}
+
+void planeAngel::privedenie0_360(){
+    while(degree<0)
+        degree+=360;
+    while(degree>360)
+        degree-=360;
+    convert();
+}
+
+void planeAngel::getSin(){
+    cout<<"Синус угла="<<sin(degree)<<endl;
+}
+
+void planeAngel:inDEcrease(bool fl, double value, char znak){
+    if(fl==0)
+        if(znak=='-')
+            radian-=(int)value;
+        else
+            radian+=(int)value;
+    else
+        if(znak=='-')
+            degree-=value;
+        else
+            degree+=value;
+}
+
+void planeAngel:diff(int radian){
+    if(this->radian==radian)
+        cout<<"Углы равны."<<endl;
+    if(this->radian<radian)
+        cout<<"Угол в объекте меньше."<<endl;
+    if(this->radian>radian)
+        cout<<"Угол в объекте больше."<<endl;
+}
+
+void planeAngel::Init(int radian, double degree){
     this->radian=radian;
     this->degree=degree;
 }
 
-int planeAngel::Read(){
-    int radian, degree;
+void planeAngel::Read(){
+    int radian;
+    double degree;
     cout<<"Задайте величину угла. Выберите, как задать угол(введите 1 или 2):"<<endl;
     cout<<" 1) в радианах;\n"<<" 2) в градусах и минутах;\n"<<":";
     int answer;
@@ -19,33 +64,22 @@ int planeAngel::Read(){
      if(answer==1){
         loop=1; 
         cout<<"Введите угол в радианах:";
-        cin>>radian;
-        //сразу метод для перевода для присвоения
-        
+        cin>>radian;        
      }
      else
          if(answer==2){
-             loop=1; 
+             loop=1;
              cout<<"Введите угол в градусах и минутах. Минуты отделяются запятой.:";
              cin>>degree;
          }
         else
             cout<<"Введен некорректный номер. Повторите попытку.";
     }
+    convert();
     Init(radian, degree);
 }
 
 void planeAngel::toString(){
-    cout<<""<<endl;
-    cout<<""<<endl;
-    cout<<""<<endl;
-}
-
-
-void planeAngel::convert(){//сделать статиком
- //из радиан в градусы
-    degree=(int)radian*180/M_PI;
-}//M_PI*degree/180;
-void planeAngel::privedenie(){
-    
+    cout<<"Угол на плоскости задан в "<<radian<<" радианах"<<endl;
+    cout<<"Угол на плоскости задан в "<<(int)degree<<"°"<<(degree-(int)degree)*100<<"'"<<endl;
 }
--- planeAngel.h
+++ planeAngel.h
@@ -1,14 +1,8 @@
 #ifndef PLANEANGEL_H
 #define PLANEANGEL_H
-/*Угол задается в градусах и минутах либо в радианах.
-Должны быть реализованы: перевод из градусов в радианы, 
-приведение к диапазону 0 – 360, 
-увеличение и уменьшение на заданную величину, 
+/*увеличение и уменьшение на заданную величину, 
  получение синуса, сравнение углов.
- 
-• метод инициализации Init();
-• ввод с клавиатуры Read();
-• преобразование в строку toString();*/
+*/
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -16,15 +10,16 @@ using namespace std;
 class planeAngel
 {
     int radian;
-    int degree;
+    double degree;
 public:
     planeAngel();
-    static void convert();//M_PI*degree/180;
-    void privedenie();
-
+    static void convert();
+    void privedenie0_360();
     void getSin();
-    void Init(int radian, int degree);
-    static int Read();
+    void inDEcrease(bool fl, double value, char znak);
+    void diff(int radian);
+    void Init(int radian, double degree);
+    static void Read();
     void toString();
 };
 
